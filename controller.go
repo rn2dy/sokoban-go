333f682c14150da0243d0936ec57503aa5dd5b66
rn2dy <qjavaswing2009@gmail.com>
2016-03-15 16:11:51 -0700

    remove busy rendering loop as suggested by Mikael

--- controller.go
+++ controller.go
@@ -25,33 +25,30 @@ func main() {
 	render(g)
 
 	for {
-		select {
-		case ev := <-eventQueue:
-			if ev.Type == termbox.EventKey {
-				switch {
-				case ev.Key == termbox.KeyArrowUp || ev.Ch == 'k':
-					g.move(UP)
-				case ev.Key == termbox.KeyArrowDown || ev.Ch == 'j':
-					g.move(DOWN)
-				case ev.Key == termbox.KeyArrowLeft || ev.Ch == 'h':
-					g.move(LEFT)
-				case ev.Key == termbox.KeyArrowRight || ev.Ch == 'l':
-					g.move(RIGHT)
-				case ev.Ch == 'n':
-					g.nextLevel()
-				case ev.Ch == 'p':
-					g.prevLevel()
-				case ev.Ch == 'r':
-					g.reset()
-				case ev.Ch == 'd':
-					g.toggleDebug()
-				case ev.Key == termbox.KeyEsc:
-					return
-				}
+		ev := <-eventQueue
+		if ev.Type == termbox.EventKey {
+			switch {
+			case ev.Key == termbox.KeyArrowUp || ev.Ch == 'k':
+				g.move(UP)
+			case ev.Key == termbox.KeyArrowDown || ev.Ch == 'j':
+				g.move(DOWN)
+			case ev.Key == termbox.KeyArrowLeft || ev.Ch == 'h':
+				g.move(LEFT)
+			case ev.Key == termbox.KeyArrowRight || ev.Ch == 'l':
+				g.move(RIGHT)
+			case ev.Ch == 'n':
+				g.nextLevel()
+			case ev.Ch == 'p':
+				g.prevLevel()
+			case ev.Ch == 'r':
+				g.reset()
+			case ev.Ch == 'd':
+				g.toggleDebug()
+			case ev.Key == termbox.KeyEsc:
+				return
 			}
-		default:
-			render(g)
-			time.Sleep(animationSpeed)
 		}
+		render(g)
+		time.Sleep(animationSpeed)
 	}
 }
